39c7ae333d6747e4e27ddad63f324b405b020488
Rob Curtis <surreymagpie@yahoo.co.uk>
2018-05-31 15:29:57 +0100

    Add devise helpers
    This provides the ability to produce a confirmed user object to test
    features requiring sign in

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "user#{n}@example.com" }
     password 'password'
+
+    trait :confirmed do
+      after(:create, &:confirm)
+    end
   end
 end
